aee6b0e4cc82a85a03d890bb3a0d2181608d4f91
Kirill Korolyov <kirill.korolyov@gmail.com>
2010-02-03 21:02:04 +0000

    Nickname updated lol

--- modules/about.php
+++ modules/about.php
@@ -6,7 +6,7 @@ if (!defined('PSXDB')) {
 }
 
 echo '<h3>Team</h3>
-<div class="textblock"><p><b>Founders</b>: LedZeppelin68, <a href="http://dremora.com/">Dremora</a><br /><b>Core members</b>: <a href="http://v.dremora.com/-v-.jpg">-v-</a>, cHrI8l3, F1ReB4LL, p_star, TeeCee, Vigi and many others...</p></div>';
+<div class="textblock"><p><b>Founders</b>: LedZeppelin68, <a href="http://dremora.com/">Dremora</a><br /><b>Core members</b>: <a href="http://v.dremora.com/-v-.jpg">-v-</a>, cHrI8l3, F1ReB4LL, p_star, TeeCee, Jackal and many others...</p></div>';
 $psxdb['title'] = 'About';
 display();
 
