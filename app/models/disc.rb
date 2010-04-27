d30e708849f6777c06188c0f61e00fe5b4668b39
Kirill Korolyov <kirill.korolyov@gmail.com>
2010-04-27 16:06:42 +0200

    Nil in Disc fields is now allowed anymore

--- app/models/disc.rb
+++ app/models/disc.rb
@@ -12,14 +12,15 @@ class Disc < ActiveRecord::Base
   attr_protected :created_at, :updated_at
   
   validates_inclusion_of :status, :in => @@statuses.keys
-  validates_length_of :internal_serial, :in => 0..255
+  validates_length_of :comments, :maximum => 65535
+  validates_length_of :internal_serial, :maximum => 255
   validates_length_of :title, :in => 1..255
-  validates_length_of :subtitle, :in => 0..255
-  validates_length_of :scene_filename, :in => 0..255
-  validates_length_of :filename, :in => 0..255
-  validates_format_of :internal_date, :with => /(19|20)[0-9][0-9]-[01][0-9](-[0123][0-9])?/, :allow_blank => true
-  validates_format_of :size, :with => /[1-9][0-9]{6,13}/, :allow_blank => true
-  validates_format_of :crc32, :with => /[0-9A-Fa-f]{8}/, :allow_blank => true
-  validates_format_of :md5, :with => /[0-9A-Fa-f]{32}/, :allow_blank => true
-  validates_format_of :sha1, :with => /[0-9A-Fa-f]{40}/, :allow_blank => true
+  validates_length_of :subtitle, :maximum => 255
+  validates_length_of :scene_filename, :maximum => 255
+  validates_length_of :filename, :maximum => 255
+  validates_format_of :internal_date, :with => /\A((19|20)[0-9][0-9]-[01][0-9](-[0123][0-9])?)?\Z/
+  validates_format_of :size, :with => /\A([1-9][0-9]{6,13})?\Z/
+  validates_format_of :crc32, :with => /\A([0-9A-Fa-f]{8})?\Z/
+  validates_format_of :md5, :with => /\A([0-9A-Fa-f]{32})?\Z/
+  validates_format_of :sha1, :with => /\A([0-9A-Fa-f]{40})?\Z/
 end
