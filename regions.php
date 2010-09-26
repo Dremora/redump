607515b0273bd7cf79b4aef4611ebab78f96033c
Kirill Korolyov <kirill.korolyov@gmail.com>
2010-09-26 15:40:52 +0300

    Added several regions and languages

--- regions.php
+++ regions.php
@@ -2,45 +2,54 @@
 
 $psxdb['regions'] = array(
 	'A'  => 'Asia',
+	'Ar' => 'Argentina',
+	'At' => 'Austria',
 	'Au' => 'Australia',
 	'B'  => 'Brazil',
 	'C'  => 'China',
 	'Cz' => 'Czech',
 	'E'  => 'Europe',
+	'EJ' => 'Europe, Japan',
 	'F'  => 'France',
-	'Fi'  => 'Finland',
+	'Fi' => 'Finland',
 	'G'  => 'Germany',
-	'Gr'  => 'Greece',
+	'Gr' => 'Greece',
 	'H'  => 'Hungary',
 	'I'  => 'Italy',
 	'J'  => 'Japan',
 	'K'  => 'Korea',
 	'N'  => 'Netherlands',
 	'P'  => 'Poland',
+	'Pt' => 'Portugal',
 	'R'  => 'Russia',
 	'S'  => 'Spain',
-	'Sw'  => 'Sweden',
+	'Sk' => 'Slovakia',
+	'Sw' => 'Sweden',
+	'UK' => 'UK',
 	'U'  => 'USA',
+	'UE' => 'USA, Europe',
+	'UJ' => 'USA, Japan',
 	'Ua' => 'Ukraine',
 	'W'  => 'World'
 );
 
 $psxdb['categories'] = array(
-	1 => 'Games',
-	2 => 'Demos',
-	3 => 'Video',
-	4 => 'Audio',
-	5 => 'Multimedia',
-	6 => 'Applications',
-	7 => 'Coverdiscs',
-	8 => 'Educational',
-	9 => 'Bonus Discs',
+	 1 => 'Games',
+	 2 => 'Demos',
+	 3 => 'Video',
+	 4 => 'Audio',
+	 5 => 'Multimedia',
+	 6 => 'Applications',
+	 7 => 'Coverdiscs',
+	 8 => 'Educational',
+	 9 => 'Bonus Discs',
 	10 => 'Betas'
 );
 
-$psxdb['languages'] = array('chi', 'hrv', 'cze', 'dan', 'dut', 'eng', 'fin', 'fre', 'ger', 'gre', 'hun', 'ita', 'jap', 'kor', 'nor', 'pol', 'por', 'rus', 'spa', 'swe');
+$psxdb['languages'] = array('ara', 'chi', 'cze', 'dan', 'dut', 'eng', 'fin', 'fre', 'ger', 'gre', 'hrv', 'hun', 'ita', 'jap', 'kor', 'nor', 'pol', 'por', 'rus', 'slk', 'spa', 'swe', 'tha');
 
 $psxdb['languages_names'] = array(
+	'ara' => 'Arabic',
 	'chi' => 'Chinese',
 	'hrv' => 'Croatian',
 	'cze' => 'Czech',
@@ -59,8 +68,10 @@ $psxdb['languages_names'] = array(
 	'pol' => 'Polish',
 	'por' => 'Portuguese',
 	'rus' => 'Russian',
+	'slk' => 'Slovak',
 	'spa' => 'Spanish',
-	'swe' => 'Swedish'
+	'swe' => 'Swedish',
+	'tha' => 'Thai'
 );
 
 $psxdb['languages_no-intro'] = array(
@@ -83,7 +94,10 @@ $psxdb['languages_no-intro'] = array(
 	'chi' => 'Ch',
 	'hrv' => 'Hr',
 	'hun' => 'Hu',
-	'cze' => 'Cs'
+	'cze' => 'Cs',
+	'slk' => 'Sk',
+	'ara' => 'Ar',
+	'tha' => 'Th'
 );
 
 ?>
